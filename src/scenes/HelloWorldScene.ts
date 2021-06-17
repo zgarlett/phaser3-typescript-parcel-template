b9e0ba4767a98e7b106e9e029e380ba4a3a55fce
zgarlett <zachary.garlett@okstate.edu>
2021-06-16 23:27:55 -0500

    Retry

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,39 +1,68 @@
 import Phaser from 'phaser'
 
+
 export default class HelloWorldScene extends Phaser.Scene
 {
+    private map: Phaser.Tilemaps.Tilemap | undefined;
+    private marker: Phaser.GameObjects.Graphics | undefined;
+    private propertiesText;
+
 	constructor()
 	{
 		super('hello-world')
-	}
+    }
 
 	preload()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.tilemapTiledJSON('map', 'assets/tile_properties.json');
+        this.load.image('tiles', 'assets/gridtiles.png');
+        
+        
     }
 
     create()
     {
-        this.add.image(400, 300, 'sky')
+        this.map = this.make.tilemap({ key: 'map' });
+        let tileset = this.map.addTilesetImage('tiles');
+        let layer = this.map.createLayer('Tile Layer 1', tileset, 0, 0);
 
-        const particles = this.add.particles('red')
+        this.marker = this.add.graphics();
+        this.marker.lineStyle(3, 0xffffff, 1);
+        this.marker.strokeRect(0, 0, this.map.tileWidth, this.map.tileHeight);
+        
+        let help = this.add.text(16, 500, 'Click on a tile to view its properties.', {
+            font: '20px Arial',
+        });
+        help.setScrollFactor(0);
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
-
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
+        this.propertiesText = this.add.text(16, 540, 'Properties: ', {
+            fontSize: '18px',
+        });
     }
+
+    update (time: number, delta: number)
+{
+    let worldPoint: Phaser.Math.Vector2 = this.input.activePointer.positionToCamera(this.cameras.main) as Phaser.Math.Vector2 || {};
+
+    // Rounds down to nearest tile
+    
+    let pointerTileX = this.map!.worldToTileX(worldPoint.x);
+    let pointerTileY = this.map!.worldToTileY(worldPoint.y);
+
+    // Snap to tile coordinates, but in world space
+    this.marker!.x = this.map!.tileToWorldX(pointerTileX);
+    this.marker!.y = this.map!.tileToWorldY(pointerTileY);
+
+    if (this.input.manager.activePointer.isDown)
+    {
+        let tile = this.map!.getTileAt(pointerTileX, pointerTileY);
+
+        if (tile)
+        {
+            // Note: JSON.stringify will convert the object tile properties to a string
+            this.propertiesText.setText('Properties: ' + JSON.stringify(tile.properties));
+            tile.properties.viewed = true;
+        }
+    }
+}
 }
